ff0c4b42eb86a1ab2696316e64b13f15725124c7
DbpGaming <LbpDbpGaming@gmail.com>
2018-11-27 21:02:21 -0600

    Main.java initial commit

---
+++ src/main/java/dbp/nextek/Main.java
@@ -0,0 +1,26 @@
+package dbp.nextek;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "nextek", name = "NexTek", version = "unfinished")
+public class Main {
+
+	@Mod.Instance("nextek")
+	public static Main instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		System.out.println("supercoder probably broke the enet");
+	}
+	
+	public void preInit(FMLInitializationEvent event){
+		System.out.println("e99999 is being a lazy bum");
+	}
+	
+	public void preInit(FMLPostInitializationEvent event){
+		System.out.println("greg still hasn't made a blast furnace yet");
+	}
+}
